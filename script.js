14bfd3263ab8365a17c0806c3bc22f8e0e4bd674
Eric Petela <epetela51@gmail.com>
2022-05-04 08:18:23 -0400

    Add functions to listen for number & operator clicks.  Convert buttons from string to numbers

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = init
+
+function init () {
+    clickingNumbers()
+
+    clickingOperators()
+}
+
 // buttons for numbers
 let numZeroBtn = document.querySelector('#zeroBtn')
 let numOneBtn = document.querySelector('#oneBtn')
@@ -8,7 +16,7 @@ let numFiveBtn = document.querySelector('#fiveBtn')
 let numSixBtn = document.querySelector('#sixBtn')
 let numSevenBtn = document.querySelector('#sevenBtn')
 let numEightBtn = document.querySelector('#eightBtn')
-let numNineBTn = document.querySelector('#nineBtn')
+let numNineBtn = document.querySelector('#nineBtn')
 
 // buttons for operators, equals & clear
 let addBtn = document.querySelector('#addBtn')
@@ -20,11 +28,65 @@ let clearBtn = document.querySelector('#clearBtn')
 
 
 // perform addition
-function addition() {
+function addition(num1, num2) {
 
 }
 
-// takes an operator (+, -, /, *) along with 2 numbers
+// takes an operator (+, -, /, *) along with 2 numbers (the number pressed BEFORE the operator click and the number pressed AFTER the operator click)
 function operate(operator, num1, num2) {
 
+}
+
+// empty array to hold numbers as they are pressed
+let numberArray = []
+
+function clickingNumbers() {
+    numZeroBtn.addEventListener('click', (e) => {
+        // var takes value from btn text and converts from string into number
+        let zero = parseInt(e.target.innerText)
+        // pushes the number from the button into an empty array
+        numberArray.push(zero)
+        
+        console.log(numberArray)
+    })
+    numOneBtn.addEventListener('click', (e) => {
+        let one = parseInt(e.target.innerText)
+        numberArray.push(one)
+        
+        console.log(numberArray)
+    })
+    numTwoBtn.addEventListener('click', (e) => {
+        let two = parseInt(e.target.innerText)
+        numberArray.push(two)
+        
+        console.log(numberArray)
+    })
+    numThreeBtn.addEventListener('click', (e) => {
+        let three = parseInt(e.target.innerText)
+        numberArray.push(three)
+        
+        console.log(numberArray)
+    })
+}
+
+function clickingOperators() {
+    addBtn.addEventListener('click', () => {
+        console.log('+')
+    })
+    subtractBtn.addEventListener('click', () => {
+        console.log('-')
+    })
+    divideBtn.addEventListener('click', () => {
+        console.log('/')
+    })
+    multipleyBtn.addEventListener('click', () => {
+        console.log('*')
+    })
+
+    equalsBtn.addEventListener('click', () => {
+        console.log('=')
+    })
+    clearBtn.addEventListener('click', () => {
+        console.log('clear')
+    })
 }
