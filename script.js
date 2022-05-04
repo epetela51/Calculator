1ae3dd6179caa09efc59a4895bcd5d51d41939aa
Eric Petela <epetela51@gmail.com>
2022-05-04 07:42:27 -0400

    added clear button to html & js

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ let numSevenBtn = document.querySelector('#sevenBtn')
 let numEightBtn = document.querySelector('#eightBtn')
 let numNineBTn = document.querySelector('#nineBtn')
 
-// buttons for operators & equals
+// buttons for operators, equals & clear
 let addBtn = document.querySelector('#addBtn')
 let subtractBtn = document.querySelector('#subtractBtn')
 let divideBtn = document.querySelector('#divideBtn')
 let multipleyBtn = document.querySelector('#multiplyBtn')
 let equalsBtn = document.querySelector('#equalsBtn')
+let clearBtn = document.querySelector('#clearBtn')
 
 
 // perform addition
