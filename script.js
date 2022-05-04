60c09eaaa217d6e4e8ca1af560c488742ab3b6ba
Eric Petela <epetela51@gmail.com>
2022-05-04 07:40:51 -0400

    added btns to html and linked btns to js

---
+++ script.js
@@ -0,0 +1,29 @@
+// buttons for numbers
+let numZeroBtn = document.querySelector('#zeroBtn')
+let numOneBtn = document.querySelector('#oneBtn')
+let numTwoBtn = document.querySelector('#twoBtn')
+let numThreeBtn = document.querySelector('#threeBtn')
+let numFourBtn = document.querySelector('#fourBtn')
+let numFiveBtn = document.querySelector('#fiveBtn')
+let numSixBtn = document.querySelector('#sixBtn')
+let numSevenBtn = document.querySelector('#sevenBtn')
+let numEightBtn = document.querySelector('#eightBtn')
+let numNineBTn = document.querySelector('#nineBtn')
+
+// buttons for operators & equals
+let addBtn = document.querySelector('#addBtn')
+let subtractBtn = document.querySelector('#subtractBtn')
+let divideBtn = document.querySelector('#divideBtn')
+let multipleyBtn = document.querySelector('#multiplyBtn')
+let equalsBtn = document.querySelector('#equalsBtn')
+
+
+// perform addition
+function addition() {
+
+}
+
+// takes an operator (+, -, /, *) along with 2 numbers
+function operate(operator, num1, num2) {
+
+}
